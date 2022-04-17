26c0eb21ddd96bd8b12dd9217d9cb742a734b0ba
ANS <36783010+answebdev@users.noreply.github.com>
2022-04-16 19:22:28 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from './components';
 import assets from './assets';
+import styles from './styles/Global';
 
 // Video: https://www.youtube.com/watch?v=_ivIUCSOZ78&t=5s
 // Code: https://github.com/adrianhajdin/nft-marketplace-showcase
@@ -8,8 +9,8 @@ const App = () => {
   return (
     <>
       <SectionWrapper
-        title='Your own store of Nifty NFTs. Start Selling & Growing.'
-        description='Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.'
+        title='You own store of Nifty NFTs. Start Selling & Growing'
+        description='Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.'
         showBtn
         mockupImg={assets.homeHero}
         banner='banner'
@@ -23,17 +24,23 @@ const App = () => {
       <Features />
       <SectionWrapper
         title='Deployment'
-        description="ProNef is built using Expo, which runs natively on all users' devices. You can easily get your app into people's hands."
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
         mockupImg={assets.feature}
         reverse
       />
       <SectionWrapper
         title='Creative way to showcase the store'
-        description='The app contains two screens. The first screen lists all NFTs, while the second one shows the details of a specific NFT.'
+        description='The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT.'
         mockupImg={assets.mockup}
         banner='banner02'
       />
       <Download />
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center'>
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className='bold'>Adolf Schmuck</span>
+        </p>
+      </div>
     </>
   );
 };
